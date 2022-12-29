1116b5b4c63e7990cc761de5eefafb9e3a81beb1
Zubin Bhaumik <113847258+ZBhaumik@users.noreply.github.com>
2022-12-29 13:28:18 -0800

    Added Validation

--- qtrader.py
+++ qtrader.py
@@ -98,8 +98,8 @@ class QTrader:
     reward = 0
     for i, stock in enumerate(self.portfolio.keys()):
       self.portfolio[stock] += actions[i]
-      self.funds += self.stock_data[stock][t] * actions[i]  # Update the funds variable
-    reward += sum(self.portfolio.values()) * (self.stock_data[stock][t+1] - self.stock_data[stock][t])
+      self.funds -= self.stock_data[stock][t] * actions[i]  # Update the funds variable
+    reward = sum([self.stock_data[stock][t] * actions[i] for i, stock in enumerate(self.portfolio.keys())])
     return reward
 
   def update_memory(self, t):
@@ -125,49 +125,69 @@ class QTrader:
 
   def trade(self, num_episodes, batch_size):
     for episode in range(num_episodes):
+      self.stock_data = train_data
       self.portfolio = self.initial_portfolio.copy()
       self.buy_sell_history = []
-      self.funds=sum([train_data[stock][0] * initial_portfolio[stock] for stock in initial_portfolio.keys()])
-
+      self.funds=0
       for t in range(len(self.stock_data["AAPL"]) - 1):
+        print(t)
         self.update_memory(t)
         if (len(self.memory) > batch_size) and (t%batch_size==0):
           self.replay(batch_size)
           self.update_target_model()  # Update target model
       self.total_profit = sum(self.portfolio.values()) - sum(self.initial_portfolio.values())
-      print(f"Episode {episode+1}: Total profit = {self.total_profit}")
+      print(f"Episode {episode+1}: Train Profit = {self.total_profit}")
+      #VALIDATION LOOP
+      self.stock_data = val_data
+      self.portfolio = self.initial_portfolio.copy()
+      self.funds=0
+      state = self.get_state(0)
+      for t in range(len(self.stock_data.index)-1):
+        actions = self.choose_actions(state, t)
+        reward = self.take_actions(actions, t)
+        state = self.get_state(t+1)
+      self.total_profit = sum(self.portfolio.values()) - sum(self.initial_portfolio.values())
+      print(f"Episode {episode+1}: Val Profit = {self.total_profit}")
+
+
+
 
   def test(self):
     self.epsilon = 0.0  # Set epsilon to 0 to disable exploration during test
-    total_profit = 0
     state = self.get_state(0)
+    self.funds=0
     self.portfolio = self.initial_portfolio.copy()  # Reset portfolio to initial values
     for t in range(len(self.stock_data.index)-1):
       actions = self.choose_actions(state, t)
       reward, done = self.take_actions(actions, t)
-      total_profit += reward
       state = self.get_state(t+1)
       if done:
         break
+    self.total_profit = sum(self.portfolio.values()) - sum(self.initial_portfolio.values())
+    print(f"Test Profit = {self.total_profit}")
 
-    print(f"Total profit: {total_profit}")
-
-
+import keras.backend as K
+import os
+def switch_k_backend_device():
+    if K.backend() == "tensorflow":
+        os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 stock_data = yfinance_retrieve(["AAPL", "GOOGL", "MSFT"], dates[0], dates[1])
 
 # Split data into training and testing sets
-split_index = int(
-    0.8 * len(stock_data)
-)  # Use 80% of data for training, 20% for testing
+split_index = int(0.8 * len(stock_data))
 train_data = stock_data.iloc[:split_index]
 test_data = stock_data.iloc[split_index:]
+split_index = int(0.5 * len(test_data))
+val_data = test_data.iloc[:split_index]
+test_data = test_data.iloc[split_index:]
 
 # Set initial portfolio
 initial_portfolio = {"AAPL": 10, "GOOGL": 5, "MSFT": 15}
 
+switch_k_backend_device()
 # Initialize StockTrader object
-trader = QTrader(train_data, initial_portfolio, funds=sum([train_data[stock][0] * initial_portfolio[stock] for stock in initial_portfolio.keys()]))
+trader = QTrader(train_data, initial_portfolio, funds=0)
 
 # Train the trader
 trader.trade(num_episodes=10, batch_size=32)
